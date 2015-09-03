944f49cfcdad48b31d2f47f33f19e322ab1b14e8
James Perkins <jperkins@redhat.com>
2015-09-03 13:38:14 -0700

    Example test injecting WildFlyContainerController

---
+++ tasks/src/test/java/org/jboss/as/quickstarts/tasks/ClientTest.java
@@ -0,0 +1,54 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.tasks;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.arquillian.api.WildFlyContainerController;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class ClientTest {
+
+    @ArquillianResource
+    private WildFlyContainerController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        if (!controller.isStarted("jboss")) {
+            controller.start("jboss");
+        }
+    }
+
+    @After
+    public void shutdown() throws Exception {
+        controller.stop("jboss");
+    }
+
+    @Test
+    public void dummy() throws Exception {
+
+    }
+}
